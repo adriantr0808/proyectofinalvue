1fcbcafa9511bc41e14abf5df35ee8aafa163978
adriantr0808 <adriantr.gt@gmail.com>
2021-11-02 14:24:30 +0100

    update

---
+++ src/components/navegacion/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+    <ul>
+      <li>Home</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+};
+</script>
+
+<style scoped>
+ul#menu li {
+  display: inline;
+  margin: 1em;
+  list-style-type: none;
+  color: blue;
+  text-decoration: none;
+}
+</style>
