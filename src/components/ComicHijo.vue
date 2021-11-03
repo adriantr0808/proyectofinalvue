225ad44210d83b4fe9c6d6abb360b014e58d8856
attiaApp <attiapp123@gmail.com>
2021-11-03 17:37:46 +0100

    update

--- src/components/ComicHijo.vue
+++ src/components/ComicHijo.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    <button>Seleccionar favorito</button>
-    <button>Modificar comic</button>
-    <button>Eliminar comic</button>
-    <h1>{{ title }}</h1>
-    <img :src="image" width="200px" />
-    <h3>{{ desc }}</h3>
+    <button @click='seleccionarFavorito(comic)'>Seleccionar favorito</button>
+    <button @click='modificarComic(index)'>Modificar comic</button>
+    <button @click='eliminarComic(index)'>Eliminar comic</button>
+    <h1>{{ comic.titulo }}</h1>
+    <img :src="comic.imagen" width="200px" />
+    <h3>{{comic.descripcion}}</h3>
   </div>
 </template>
 <script>
 export default {
   name: "ComicHijo",
-  props: ["title", "image", "desc"],
-};
+  methods:{
+    seleccionarFavorito(comicfav){
+      
+     this.$emit("seleccionarFavParent",comicfav);
+     console.log(comicfav);
+  },
+
+  eliminarComic(index){
+     this.$emit("eliminarComicParent",index);
+  },
+
+  modificarComic(index){
+     this.$emit("modificarComicParent",index);
+  }
+  
+  
+ 
+},
+ props: ["comic","index"],
+}
 </script>
+<style scoped>
+  button{
+    margin:1em;
+  }
+</style>
